39df825a1f21b58992a563027ec851723229a47c
haohan <haohan11per@protonmail.com>
2023-07-12 20:56:47 +0800

    add insert tag script and lesson categories data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const db = require(__dirname + "/modules/connectDB.js");
+const db = require(__dirname + '/modules/connectDB.js');
 const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
