959362247695f71714d642e89a9fbc5c8420851c
haohan <haohan11per@protonmail.com>
2023-07-21 17:50:21 +0800

    update tags table name

--- routes/lesson.js
+++ routes/lesson.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/tags', async (req, res) => {
-  const sql = `SELECT name FROM c_l_tag ORDER BY sid`;
+  const sql = `SELECT name FROM c_l_tags ORDER BY sid`;
   const [datas] = await db.query(sql);
   const tags = datas.map((data) => data.name);
   res.json(tags);
