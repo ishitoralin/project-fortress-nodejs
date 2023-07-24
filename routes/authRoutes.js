2a2306b8773f9c6ad2b86447b44fbe066e2d1ce8
LQTjim <jim6050482@hotmail.com>
2023-07-25 07:05:52 +0800

    test:middleware protect *should be rename

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,57 +7,78 @@ require('dayjs/locale/zh-tw');
 const path = require('path');
 require('dotenv').config();
 const router = express.Router();
+const protect = async (req, res, next) => {
+  const auth = req.get('Authorization');
+  console.log(auth);
+  if (auth && auth.indexOf('Bearer ') === 0) {
+    const token = auth.slice(7);
+    let decoded = null;
 
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  let rows;
-  [rows] = await db.query(`SELECT * FROM member WHERE email = '${email}'`);
-  let user;
-  if (rows.length > 0) {
-    user = rows[0];
-    //驗證資料庫拉出來的資料 的密碼和 下面密碼是不是一樣
-    const result = await bcrypt.compare(password, user.password);
-    if (!result) {
-      return res.status(401).json({ code: 401, message: '帳號或密碼錯誤' });
-    }
-    //jwt
-    //成功就放入JWT accessToken 和 refreshToken
-    console.log('使用者資料正確');
-    const accessToken = jwt.sign(
-      {
-        id: user.sid,
-        name: user.name,
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '60m' }
-    );
-    const refreshToken = jwt.sign(
-      { id: user.id, username: user.username },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: '60d' }
-    );
-    //放入refreshToken進httponly cookie
-    res.cookie('g4RefreshToken', refreshToken, {
-      maxAge: 5184000000,
-      httpOnly: true,
-    });
-
-    //放入accessToken進json 前端接住丟進state內
-    user.hero_icon = `http://localhost:${process.env.PORT}/imgs/member/${user.hero_icon}`;
-    return res.status(200).json({
-      code: 200,
-      accessToken,
-      user: {
-        id: user.sid,
-        name: user.name,
-        role: user.role_sid,
-        icon: user.hero_icon,
-      },
-      message: '登入成功',
-    });
+    decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log(decoded.id);
+    // const currentUser
   }
-  //TODO
-  res.status(401).json({ code: 401, message: '帳號或密碼錯誤' });
-});
+
+  // req.locals.user =user;
+
+  next();
+};
+router
+  .get('/test', protect, (req, res) => {
+    res.json({ data: 123 });
+  })
+  .post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    console.log(req.body);
+    console.log(email, password);
+    let rows;
+    [rows] = await db.query(`SELECT * FROM member WHERE email = '${email}'`);
+    let user;
+    if (rows.length > 0) {
+      user = rows[0];
+      //驗證資料庫拉出來的資料 的密碼和 下面密碼是不是一樣
+      const result = await bcrypt.compare(password, user.password);
+      if (!result) {
+        return res.status(401).json({ code: 401, message: '帳號或密碼錯誤' });
+      }
+      //jwt
+      //成功就放入JWT accessToken 和 refreshToken
+      console.log('使用者資料正確');
+      const accessToken = jwt.sign(
+        {
+          id: user.sid,
+          name: user.name,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '60m' }
+      );
+      const refreshToken = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '60d' }
+      );
+      //放入refreshToken進httponly cookie
+      res.cookie('g4RefreshToken', refreshToken, {
+        maxAge: 5184000000,
+        httpOnly: true,
+      });
+
+      //放入accessToken進json 前端接住丟進state內
+      user.hero_icon = `http://localhost:${process.env.PORT}/imgs/member/${user.hero_icon}`;
+      return res.status(200).json({
+        code: 200,
+        accessToken,
+        user: {
+          id: user.sid,
+          name: user.name,
+          role: user.role_sid,
+          icon: user.hero_icon,
+        },
+        message: '登入成功',
+      });
+    }
+    //TODO
+    res.status(401).json({ code: 401, message: '帳號或密碼錯誤' });
+  });
 module.exports = router;
+exports.protect;
--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -8,7 +8,7 @@ router
   .get('/', async (req, res) => {
     // req.locals.user = {sid:1,};
     // const { id } = req.locals.user;
-    let id = 1;
+    let id = 5;
     if (!id) {
       return res.status(404).json({ code: 404, message: '沒有資料' });
     }
@@ -26,9 +26,40 @@ router
     }
   })
   .post('/', (req, res, next) => {})
-  .get('/member-favorite-courses', (req, res, next) => {})
-  .delete('/member-favorite-courses', (req, res, next) => {})
-  .get('/member-favorite-products', (req, res, next) => {})
+  .get('/member-favorite-courses', async (req, res, next) => {})
+  .delete('/member-favorite-courses', async (req, res, next) => {})
+  .get('/member-favorite-products', async (req, res, next) => {
+    let id = 5;
+    let sql = `SELECT CASE
+    WHEN mfp.category_sid = 1 THEN pn.product_name
+    WHEN mfp.category_sid = 2 THEN fn.food_name
+    WHEN mfp.category_sid = 3 THEN en.equipment_name
+       END AS name  ,
+CASE
+    WHEN mfp.category_sid = 1 THEN pn.picture
+    WHEN mfp.category_sid = 2 THEN fn.picture
+    WHEN mfp.category_sid = 3 THEN en.picture
+       END AS picture  ,
+CASE
+    WHEN mfp.category_sid = 1 THEN pn.price
+    WHEN mfp.category_sid = 2 THEN fn.price
+    WHEN mfp.category_sid = 3 THEN en.price
+       END AS price 
+FROM member_favorite_products  AS mfp
+LEFT JOIN product_name AS pn ON mfp.product_sid = pn.sid AND mfp.category_sid = 1
+LEFT JOIN food_name AS fn ON mfp.product_sid = fn.sid AND mfp.category_sid = 2
+LEFT JOIN equipment_name AS en ON mfp.product_sid = en.sid AND mfp.category_sid = 3
+WHERE mfp.member_sid =${id}
+`;
+    let rows;
+    [rows] = await db.query(sql);
+    if (rows.length > 0) {
+      return res.status(200).json({ code: 200, rows, message: '有資料' });
+    }
+    console.log(rows.length);
+
+    return res.status(404).json({ code: 404, message: '沒有資料' });
+  })
   .delete('/member-favorite-products', (req, res, next) => {})
 
   .use('*', (req, res) => {
