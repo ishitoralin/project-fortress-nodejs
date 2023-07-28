11b7e794bbe1020cbd1c5ffb1f8b2c8ceb108374
shihu <miraculous0218@hotmail.com>
2023-07-28 16:25:36 +0800

    SQL finished

--- routes/cart.js
+++ routes/cart.js
@@ -8,25 +8,58 @@ const router = express.Router();
 // });
 
 // router.get('/cart', ({ query: { sid } }, res) => {
-router.get('/:sid', (req, res) => {
+router.get('/:sid', async (req, res) => {
   const sid = req.params?.sid;
-  console.log('first');
-  console.log(isNaN(sid), 'L14');
-  console.log(!sid, 'L14');
   if (!sid || isNaN(sid)) {
     return res.status(404).json({ error: '無效的id' });
   }
 
-  const query = 'SELECT * FROM `order_cart` WHERE member_sid = ?';
+  const query = `SELECT
+  oc.sid,
+  oc.member_sid,
+  oc.products_type_sid,
+  oc.item_sid,
+  oc.quantity,
+  oc.created_at,
+  CASE
+      WHEN oc.products_type_sid = 1 THEN pn.product_name
+      WHEN oc.products_type_sid = 2 THEN fn.food_name
+      WHEN oc.products_type_sid = 3 THEN en.equipment_name
+      WHEN oc.products_type_sid = 4 THEN ln.name
+      ELSE NULL
+  END AS item_name,
+  CASE
+      WHEN oc.products_type_sid = 1 THEN pn.price
+      WHEN oc.products_type_sid = 2 THEN fn.price
+      WHEN oc.products_type_sid = 3 THEN en.price
+      WHEN oc.products_type_sid = 4 THEN ln.price
+      ELSE NULL
+  END AS price,
+  CASE
+      WHEN oc.products_type_sid = 1 THEN pn.picture
+      WHEN oc.products_type_sid = 2 THEN fn.picture
+      WHEN oc.products_type_sid = 3 THEN en.picture
+      WHEN oc.products_type_sid = 4 THEN ln.category_sid
+      ELSE NULL
+  END AS picture
+FROM
+  order_cart AS oc
+  LEFT JOIN product_name AS pn ON oc.products_type_sid = 1
+  AND oc.item_sid = pn.sid
+  LEFT JOIN food_name AS fn ON oc.products_type_sid = 2
+  AND oc.item_sid = fn.sid
+  LEFT JOIN equipment_name AS en ON oc.products_type_sid = 3
+  AND oc.item_sid = en.sid
+  LEFT JOIN c_l_lessons AS ln ON oc.products_type_sid = 4
+  AND oc.item_sid = ln.sid
+WHERE
+  oc.member_sid = 5;`;
 
-  db.query(query, [sid])
-    .then((results) => {
-      res.status(200).json(results);
-    })
-    .catch((err) => {
-      console.error('Error executing query:', err);
-      res.status(500).json({ error: 'Error fetching data from database' });
-    });
+  let rows;
+  [rows] = await db.query(query, [sid]);
+
+  const data = rows;
+  res.status(200).json({ code: 200, data });
 });
 
 module.exports = router;
