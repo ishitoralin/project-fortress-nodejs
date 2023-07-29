c0ac53547161fb7e3f45a54cc05eb431d39dd3b2
HaoHan <haohan11pfer@protonmail.com>
2023-07-30 03:15:59 +0800

    add coach api

--- routes/coach.js
+++ routes/coach.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
+  if (isNaN(parseInt(id))) return res.json({ message: 'Invalid params' });
   const sql = `SELECT * FROM c_l_coachs WHERE sid = ${id}`;
   const [[data]] = await db.query(sql);
 
@@ -12,10 +13,36 @@ router.get('/:id', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-  const sql = 'SELECT sid FROM c_l_coachs WHERE 1';
-  const [datas] = await db.query(sql);
+  const { location } = req.query;
 
-  res.json(datas.map((data) => data.sid));
+  if (location === undefined) {
+    const sql = 'SELECT sid FROM c_l_coachs WHERE 1';
+    const [datas] = await db.query(sql);
+
+    return res.json(datas.map((data) => data.sid));
+  }
+
+  const sids = await getLocationsSid(
+    Array.isArray(location) ? location : [location]
+  );
+
+  const sql = `SELECT c.*, l.name as location FROM c_l_coachs c JOIN c_l_location l ON c.location_sid = l.sid WHERE location_sid IN (
+    ${Array(sids.length).fill('?').join(',')})`;
+
+  const [data] = await db.query(sql, sids);
+  res.json(data);
 });
+
+const getLocationsSid = async (locations) => {
+  const sids = await Promise.all(
+    locations.map(async (location) => {
+      const sql = 'SELECT sid FROM c_l_location WHERE name = ?';
+      const [[data]] = await db.query(sql, location);
+      return data !== undefined ? data.sid : -1;
+    })
+  );
+
+  return sids.filter((sid) => sid !== -1);
+};
 
 module.exports = router;
