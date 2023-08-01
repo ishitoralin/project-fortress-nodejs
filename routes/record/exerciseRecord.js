205cb102b8efb5f870ebcab31941909769303768
Sean <x90417@gmail.com>
2023-08-01 15:41:04 +0800

    update exercise route

--- routes/record/exerciseRecord.js
+++ routes/record/exerciseRecord.js
@@ -6,7 +6,7 @@ const moment = require('moment-timezone');
 // ===============================================================
 // === exercise ==================================================
 // ===============================================================
-//>>> get all exercise record
+//>>> get all exercise record =======================================================================
 router.get(
   '/exercise-record/:start?/:end?',
   // >>> member middleware
@@ -64,7 +64,7 @@ router.get(
 );
 //<<< get all exercise record
 
-// >>> add exercise record
+// >>> add exercise record =======================================================================
 router.post('/add-record', async (req, res, next) => {
   let mID = 5;
   const { typeID, quantity, sets, reps, date } = req.body;
@@ -79,7 +79,7 @@ router.post('/add-record', async (req, res, next) => {
 });
 // <<< add exercise record
 
-// >>> delete exercise record for one centain date
+// >>> delete exercise record for one centain date =======================================================================
 router.delete('/delete-record', async (req, res, next) => {
   // FIXME:temporate member sid
   let mID = 5;
@@ -105,4 +105,84 @@ router.delete('/delete-record', async (req, res, next) => {
 });
 // <<< delete exercise record for one centain date
 
+// >>> get exercise for plot =======================================================================
+router.get(
+  '/exercise-record-plot/:start/:end/:exeSid',
+  // >>> member middleware
+  function (req, res, next) {
+    res.locals.memberId = 5;
+    next();
+  },
+  //<<<   // >>> member middleware
+  async (req, res) => {
+    const output = {
+      success: false,
+      error: '',
+      data: null,
+    };
+    // FIXME:temporate member sid
+    const sid = parseInt(res.locals.memberId);
+    const exeSid = req.params.exeSid || 0;
+    if (!sid) {
+      output.error = 'wrong id';
+      return res.status(200).json(output);
+    }
+    if (!exeSid) {
+      output.error = 'wrong exercsie id';
+      return res.status(200).json(output);
+    }
+    const start = req.params.start || 0;
+    const end = req.params.end || 0;
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    if (!start || !end || startDate > endDate) {
+      output.error = 'wrong dates';
+      return res.status(200).json(output);
+    }
+
+    const fm = 'YYYY-MM-DD';
+    // old select :SELECT er.sid, er.member_sid, er.exe_type_sid AS typeID, et.exercise_name AS name, er.weight AS quantity, er.sets, er.reps, DATE(er.exe_date) AS date
+    let sql = `SELECT er.member_sid, er.exe_type_sid AS typeID, et.exercise_name AS name, er.weight AS quantity, er.sets, er.reps, DATE(er.exe_date) AS date
+    FROM record_exercise_record as er
+    JOIN record_exercise_type as et ON er.exe_type_sid = et.sid
+    WHERE er.member_sid = ${sid} AND er.exe_type_sid = ${exeSid} AND DATE(er.exe_date) BETWEEN '${start}' AND '${end}'
+    ORDER BY er.exe_date DESC;`;
+    let [rows] = await db.query(sql);
+
+    if (!rows.length) {
+      output.error = 'no data';
+      return res.status(200).json(output);
+    }
+
+    rows = rows.map((row) => {
+      row.date = moment(row.date).format(fm);
+      // calc volumn
+      row = {
+        ...row,
+        volumn: Number(row.quantity * row.reps * row.sets).toFixed(1),
+      };
+      return row;
+    });
+
+    rows = rows.reduce((acc, cur) => {
+      const { member_sid, typeID, name, date, volumn } = cur;
+      const curObj = { member_sid, typeID, name, date, volumn };
+      const existObj = acc.find((obj) => obj.date === curObj.date);
+      if (existObj) {
+        existObj.volumn = Number(curObj.volumn) + Number(existObj.volumn);
+      } else {
+        acc.push(curObj);
+      }
+      return acc;
+    }, []);
+
+    // console.log(rows);
+    output.success = true;
+    output.data = rows;
+    res.json(output);
+  }
+);
+
+// <<< get exercise for plot
 module.exports = router;
