8b2b34857cfd187e95d7647b64936a868ca5e6ce
shihu <miraculous0218@hotmail.com>
2023-08-02 11:09:13 +0800

    a

--- routes/cart.js
+++ routes/cart.js
@@ -4,8 +4,10 @@ require('dayjs/locale/zh-tw');
 const router = express.Router();
 // 從資料庫抓取資料，購物車顯示商品
 // postman用get
-router.get('/:sid', async (req, res) => {
-  const sid = req.params?.sid;
+const { protect } = require(__dirname + '/../modules/auth.js');
+router.use(protect);
+router.get('/', async (req, res) => {
+  const { sid } = res.locals.user;
   if (!sid || isNaN(sid)) {
     return res.status(404).json({ error: '無效的id' });
   }
@@ -52,7 +54,7 @@ FROM
   WHERE l.category_sid = c.sid) AS ln ON oc.products_type_sid = 4
   AND oc.item_sid = ln.sid
 WHERE
-  oc.member_sid = 5;`;
+  oc.member_sid = ?;`;
 
   let rows;
   [rows] = await db.query(query, [sid]);
