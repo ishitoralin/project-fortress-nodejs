95356f4e4ac855ae67d154543b485fd161de5f43
Sean <x90417@gmail.com>
2023-08-09 22:27:25 +0800

    add plot-max

--- routes/record/exerciseRecord.js
+++ routes/record/exerciseRecord.js
@@ -114,22 +114,15 @@ router.delete('/delete-record', async (req, res, next) => {
 });
 // <<< delete exercise record for one centain date
 
-// >>> get exercise for plot =======================================================================
+// >>> get exercise for plot volumn =======================================================================
 router.get(
-  '/exercise-record-plot/:start/:end/:exeSid',
-  // >>> member middleware
-  // function (req, res, next) {
-  //   res.locals.memberId = 5;
-  //   next();
-  // },
-  //<<<   // >>> member middleware
+  '/exercise-record-plot-volumn/:start/:end/:exeSid',
   async (req, res) => {
     const output = {
       success: false,
       error: '',
       data: 0,
     };
-    // const sid = parseInt(res.locals.memberId);
 
     const { sid: mID } = res.locals.user; // add auth
     const exeSid = req.params.exeSid || 0;
@@ -152,7 +145,7 @@ router.get(
     }
 
     const fm = 'YYYY-MM-DD';
-    // old select :SELECT er.sid, er.member_sid, er.exe_type_sid AS typeID, et.exercise_name AS name, er.weight AS quantity, er.sets, er.reps, DATE(er.exe_date) AS date
+
     let sql = `SELECT er.member_sid, er.exe_type_sid AS typeID, et.exercise_name AS name, er.weight AS quantity, er.sets, er.reps, DATE(er.exe_date) AS date
     FROM record_exercise_record as er
     JOIN record_exercise_type as et ON er.exe_type_sid = et.sid
@@ -175,6 +168,7 @@ router.get(
       return row;
     });
     //<<< 計算volumn, 加入rows
+    // console.log(rows);
 
     //>>> 累加同一天的相同運動volumn
     rows = rows.reduce((acc, cur) => {
@@ -195,5 +189,80 @@ router.get(
   }
 );
 
-// <<< get exercise for plot
+// <<< get exercise for plot volumn
+
+// >>> get exercise for plot max =======================================================================
+router.get(
+  '/exercise-record-plot-max/:start/:end/:exeSid',
+  async (req, res) => {
+    const output = {
+      success: false,
+      error: '',
+      data: 0,
+    };
+
+    const { sid: mID } = res.locals.user; // add auth
+    const exeSid = req.params.exeSid || 0;
+    if (!mID) {
+      output.error = 'wrong id';
+      return res.status(200).json(output);
+    }
+    if (!exeSid) {
+      output.error = 'wrong exercsie id';
+      return res.status(200).json(output);
+    }
+    const start = req.params.start || 0;
+    const end = req.params.end || 0;
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    if (!start || !end || startDate > endDate) {
+      output.error = 'wrong dates';
+      return res.status(200).json(output);
+    }
+
+    const fm = 'YYYY-MM-DD';
+
+    let sql = `SELECT er.member_sid, er.exe_type_sid AS typeID, et.exercise_name AS name, er.weight AS quantity, er.sets, er.reps, DATE(er.exe_date) AS date
+    FROM record_exercise_record as er
+    JOIN record_exercise_type as et ON er.exe_type_sid = et.sid
+    WHERE er.member_sid = ${mID} AND er.exe_type_sid = ${exeSid} AND DATE(er.exe_date) BETWEEN '${start}' AND '${end}'
+    ORDER BY er.exe_date DESC;`;
+    let [rows] = await db.query(sql);
+
+    if (!rows.length) {
+      output.error = 'no data';
+      return res.status(200).json(output);
+    }
+
+    //>>> 改變date的format
+    rows = rows.map((row) => {
+      return { ...row, date: moment(row.date).format(fm) };
+    });
+    //<<< 改變date的format
+
+    // >>> 回傳只有當天max重量的數值
+
+    rows = rows.reduce((result, obj) => {
+      const existingObj = result.find((item) => item.date === obj.date);
+      if (!existingObj || obj.quantity > existingObj.quantity) {
+        const newObj = { ...obj };
+        if (existingObj) {
+          console.log('here');
+          result.splice(result.indexOf(existingObj), 1, newObj); // obj.quantity > existingObj.quantity
+        } else {
+          result.push(newObj); // no existingObj
+        }
+      }
+
+      return result;
+    }, []);
+    //<<<回傳只有當天max重量的數值
+    output.success = true;
+    output.data = rows;
+    res.json(output);
+  }
+);
+
+// <<< get exercise for plot max
 module.exports = router;
